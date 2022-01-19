ef57b9acd4370790bf8b4cc53996d6029fe2789e
Kavya Kunder <81958043+kavyakunder@users.noreply.github.com>
2022-01-20 01:24:37 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const jokeElement = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+// generateJoke();
+jokeBtn.addEventListener("click", generateJoke);
+
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  fetch("https://icanhazdadjoke.com", config)
+    .then((response) => response.json())
+    .then((data) => (jokeElement.innerHTML = data.joke));
+}
